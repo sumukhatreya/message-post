08ad0e3a862da2a755ad3146d93ced4601764792
Sumukh Atreya <atreya.sumukh@gmail.com>
2022-09-06 12:05:44 +0530

    Posts, auth and header

--- client/src/app.js
+++ client/src/app.js
@@ -2,7 +2,9 @@ const myApp = angular.module('messagePost', ['ngRoute']);
 
 myApp.config([
   '$routeProvider',
-  function ($routeProvider) {
+  '$locationProvider',
+  function ($routeProvider, $locationProvider) {
+    // $locationProvider.html5Mode(true);
     $routeProvider
       .when('/login', {
         templateUrl: 'views/login.html',
@@ -12,8 +14,9 @@ myApp.config([
         templateUrl: 'views/register.html',
         controller: 'registrationController',
       })
-      .when('/home', {
-        templateUrl: 'views/home.html',
+      .when('/posts', {
+        templateUrl: 'views/posts.html',
+        controller: 'postController',
       })
       .otherwise({
         redirectTo: '/login',
@@ -28,12 +31,20 @@ myApp.controller('loginController', [
   function ($scope, $http, $location) {
     $scope.isUserLoggedIn = false;
 
-    $http
-      .get('http://localhost:9000/login')
+    // const jwtToken = sessionStorage.getItem('jwt');
+
+    const configGet = {
+      method: 'GET',
+      url: 'http://localhost:9000/login',
+      headers: {
+        jwt: sessionStorage.getItem('jwt'),
+      },
+    };
+
+    $http(configGet)
       .then((res) => {
-        if (res.status === 200) {
-          $location.path('home');
-        }
+        console.log('Going home');
+        $location.path('posts');
       })
       .catch((err) => {
         $scope.isUserLoggedIn = true;
@@ -43,7 +54,7 @@ myApp.controller('loginController', [
     $scope.submitLoginForm = () => {
       console.log('Inside the submit form function');
 
-      const config = {
+      const configPost = {
         method: 'POST',
         url: 'http://localhost:9000/login',
         data: JSON.stringify($scope.user),
@@ -53,10 +64,11 @@ myApp.controller('loginController', [
         withCredentials: true,
       };
 
-      $http(config)
+      $http(configPost)
         .then((res) => {
           if (res.status === 200) {
-            $location.path('home');
+            sessionStorage.setItem('jwt', res.headers('jwt'));
+            $location.path('posts');
           }
         })
         .catch((err) => console.log(err));
@@ -84,13 +96,67 @@ myApp.controller('registrationController', [
       $http(config)
         .then((res) => {
           if (res.status === 201) {
-            $location.path('home');
+            sessionStorage.setItem('jwt', res.headers('jwt'));
+            $location.path('posts');
           }
         })
         .catch((err) => {
           $scope.errMsg = err.statusText;
           console.log(err);
         });
+    };
+  },
+]);
+
+myApp.controller('postController', [
+  '$scope',
+  '$http',
+  '$location',
+  function ($scope, $http, $location) {
+    if (!sessionStorage.getItem('jwt')) {
+      $location.path('login');
+    }
+    $scope.posts = [];
+
+    const configGet = {
+      method: 'GET',
+      url: 'http://localhost:9000/posts',
+      headers: {
+        'Content-Type': 'application/json',
+        jwt: sessionStorage.getItem('jwt'),
+      },
+    };
+
+    $http(configGet)
+      .then((res) => {
+        console.log('Hello from the postController GET', res.data.posts);
+        $scope.posts = res.data.posts.reverse();
+      })
+      .catch((err) => console.log(err));
+
+    $scope.submitPostForm = () => {
+      const config = {
+        method: 'POST',
+        url: 'http://localhost:9000/posts',
+        data: JSON.stringify($scope.post),
+        headers: {
+          'Content-Type': 'application/json',
+          jwt: sessionStorage.getItem('jwt'),
+        },
+      };
+      $http(config)
+        .then((res) => {
+          console.log(res);
+          $scope.posts.unshift(res.data);
+          $scope.post.title = '';
+          $scope.post.message = '';
+        })
+        .catch((err) => console.log(err));
+    };
+
+    $scope.logoutUser = () => {
+      sessionStorage.clear();
+      $location.path('/login');
     };
   },
 ]);
